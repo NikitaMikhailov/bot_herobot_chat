c66e80d75877535e60a28b5c42c8c631774fe22b
Mikhailov Nikita <44605908+NikitaMikhailov@users.noreply.github.com>
2019-07-22 14:54:40 +0300

    Update bot_chat.py

--- bot_chat.py
+++ bot_chat.py
@@ -72,7 +72,7 @@ for i in range(2,10):
 def goroscop1():
     spisok_znakov=['aries','taurus','gemini','cancer','leo','virgo','libra','scorpio','sagittarius','capricorn','aquarius','pisces']
     for i in range (0,12):
-        filegor=open('goroskop_files/'+spisok_znakov[i]+'.txt','w')
+        filegor=open('goroskop_files/'+spisok_znakov[i]+'.txt','w', encoding="utf-8")
         filegor.write(((bs4.BeautifulSoup(requests.get("http://astroscope.ru/horoskop/ejednevniy_goroskop/" + spisok_znakov[i] + ".html").text,"html.parser").find('div', 'col-12')).getText().lstrip()))
         filegor.close()
 
