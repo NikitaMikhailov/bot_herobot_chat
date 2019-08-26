6fd06ffb54484753cdcf23240f4c7c475f1b1a15
Mikhailov Nikita <44605908+NikitaMikhailov@users.noreply.github.com>
2019-08-26 19:12:52 +0300

    Update bot_chat.py

--- bot_chat.py
+++ bot_chat.py
@@ -514,7 +514,7 @@ def mainfunc():
                         vk.messages.send(  # Отправляем собщение
                             chat_id=event.chat_id,
                             random_id=get_random_id(),
-                            message='Антон вернётся к нам через ' + str(dateAntonfinish) + ' ' + Antontime(dateAntonfinish) + '\nОн уже служит ' + str(dateAntonstart) + ' ' + Antontime(dateAntonstart) + '\nУже прошло ' + str(int(dateAntonstart) // 3.66)[:-2:] + '% Aрмии.'                                                                    
+                            message='Антон вернётся к нам через ' + str(dateAntonfinish) + ' ' + Antontime(dateAntonfinish) + '\nОн уже служит ' + str(dateAntonstart) + ' ' + Antontime(dateAntonstart) + '\nУже прошло ' + str(int(dateAntonstart) // 3.66)[:-2:] + '% службы.'                                                                    
                         )
 
                     elif event.obj.text == 'бот мысль' or event.obj.text == 'мысль' and flkv == True or event.obj.text == 'мысль' and flkv2 == True:
