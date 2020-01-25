ade07df85c0e066f2d484ff009afd927e94019e2
NikitaMikhailov <mikhailov-nikita1997@yandex.ru>
2020-01-25 12:47:41 +0300

    Up_25_01_2

--- random_day.py
+++ random_day.py
@@ -15,33 +15,15 @@ longpoll = VkBotLongPoll(vk_session, '178949259')
 vk = vk_session.get_api()
 upload = VkUpload(vk_session)
 
-vk.messages.send(
-    user_id=195310233,
-    random_id=get_random_id(),
-    message='11'
-)
-
 spisok_chata = {195310233:"Никита Михайлов",38375746:"Антон Фокин",120727528:"Ольга Меркулова",51556033:"Петр Евдокимов",
                   13069991:"Андрей Петранов",20765196:"Катя Горюнова",109828457:"Александр Маслов",206947265:"Fidl Di-Di",
                   12403758:"Вика Карпеева",135053737:"Анастасия Живых"}
 pidor_id = random.choice(list(spisok_chata.keys()))
 pidor = spisok_chata[pidor_id]
 
-vk.messages.send(
-    user_id=195310233,
-    random_id=get_random_id(),
-    message='22'
-)
-
 f1 = open('/root/bot_herobot_chat/resurses/pidors.txt', 'a')
 f1.write(str(pidor_id)+'\n')
 f1.close()
-
-vk.messages.send(
-    user_id=195310233,
-    random_id=get_random_id(),
-    message='33'
-)
 
 f1 = open('/root/bot_herobot_chat/resurses/pidor_today.txt', 'w')
 f1.write(str(pidor))
@@ -60,20 +42,20 @@ photo = upload.photo_messages(photos=image.raw)[0]
 attachments.append('photo{}_{}'.format(photo['owner_id'], photo['id']))
 
 vk.messages.send(
-    chat_id=5,
+    chat_id=1,
     random_id=get_random_id(),
     attachment=','.join(attachments),
     message="Так-так-так, у нас ежедневная рубрика!"
 )
 time.sleep(10)
 vk.messages.send(
-    chat_id=5,
+    chat_id=1,
     random_id=get_random_id(),
     message="Вот это неожиданность, оказывается, что ..."
 )
 time.sleep(10)
 vk.messages.send(
-    chat_id=5,
+    chat_id=1,
     random_id=get_random_id(),
     message="[id"+str(pidor_id)+'|'+pidor+"] Ты пидор дня."
 )
