83484d9536293216db977acb120a0736537cf719
NikitaMikhailov <mikhailov-nikita1997@yandex.ru>
2020-02-05 15:12:29 +0300

    Up_05_02_2

--- bot_chat.py
+++ bot_chat.py
@@ -782,7 +782,7 @@ def mainfunc():
                             message=mat
                         )
 
-                    elif (event.obj.text == '!маты' or event.obj.text == '! маты') and event.obj.from_id == 195310233:
+                    elif (event.obj.text == '!маты' or event.obj.text == '! маты'):
                         f = open('resurses/mat.txt', 'r')
                         dism = {}
                         for line in f:
